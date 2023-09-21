44d19cd56279c2873532cec577776315252f6495
RheseSoemo <RheseSoemo.SocialMedia@outlook.com>
2023-09-21 20:01:04 +0000

    Hooked Positions into GameMap

--- src/levelup/GameMap.py
+++ src/levelup/GameMap.py
@@ -1,11 +1,23 @@
 from levelup.controller import Direction
+from levelup.position import Position
+import array
 
 class GameMap:
-    NumberOfPosition = 100
+    BoardWidth = 10
+    ArrayX = []
+    ArrayY = []
     # define position []
 
     def __init__(self, number):
         self.NumberOfPosition = number
+        for x in range(0, number):
+            position: Position = Position(1)
+            self.ArrayX.append(position)
+        for y in range(0,number):
+            position: Position = Position(1)
+            self.ArrayY.append(position)
+
+
 
     # def getPosition()
     def calculatePosition(direction: Direction):
--- tests/test_GameMap.py
+++ tests/test_GameMap.py
@@ -3,6 +3,6 @@ from levelup.GameMap import GameMap
 
 class TestGameMap(TestCase):
     def test_init(self):
-        testObj = GameMap(100)
-        assert testObj.get_total_positions() == 100
+        testObj = GameMap(10)
+        assert testObj.get_total_positions() == 10
         
